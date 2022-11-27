4a6090d9109cd0600e7302d26962aa20f96f7e70
Mason Twitty <mason.twitty@gmail.com>
2022-11-26 17:41:04 -0800

    cleanup unnecessary code

--- movies-app/src/components/Modal.jsx
+++ movies-app/src/components/Modal.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import './Modal.css'
-// import MovieDetails from './movieDetails'
 
 function Modal(props) {
   const handleClick = (e) => {
-    e.stopPropagation()
     props.onClose()
   }
   return (
